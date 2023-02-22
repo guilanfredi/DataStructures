2926da078d9042a882c531d520b1d17310cbf68e
Guilherme Carvalho <gui.lanfredi@gmail.com>
2023-02-22 17:19:14 -0300

    Add project files.

---
+++ DataStructures/Arrays/ArrayUtils.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    internal static class ArrayUtils
+    {
+        /// <summary>
+        /// Encontra o valor da posição do meio do vetor. Caso o vetor tenha tamanho par, considera o indice arredondado para baixo.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int FindMiddleValue(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Encontra no vetor a primeira ocorrência do valor suprido como parâmetro e retorna o índice desse valor.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int FindFirst(int[] array, int target)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Encontra no vetor a última ocorrência do valor suprido como parâmetro e retorna o índice desse valor.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static int FindLast(int[] array, int target)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Encontra o índice do menor valor dentro do vetor passado como parâmetro
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static int Min(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Encontra o índice do maior valor dentro do vetor passado como parâmetro
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static int Max(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Encontra a média entre os valores dentro do vetor
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static double Average(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Ordena o vetor passado como parâmetro
+        /// </summary>
+        /// <param name="array"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void Sort(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Realiza busca binária no array e retorna o índice do valor encontrado
+        /// </summary>
+        /// <param name="array"></param>
+        public static int Search(int[] array, int target)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //--------------------------------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------- DESAFIO ------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Retorna o vetor que representa a união entre 2 vetores passados como parâmetro (Assume que os vetores estão ordenados)
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static int[] Union(int[] array1, int[] array2)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retorna o vetor que representa a intersecção entre 2 vetores passados como parâmetro (Assume que os vetores estão ordenados)
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static int[] Intersection(int[] array1, int[] array2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataStructures.Arrays;
+
+int[] array = new int[7] { 5, 3, 1, 9, 4, 8, 1 };
+int[] arrayGrandePraCaramba = new int[100] { 61, 15, 65, 82, 17, 1, 36, 34, 18, 19, 7, 50, 36, 8, 31, 5, 
+    9, 57, 62, 32, 64, 92, 30, 92, 4, 9, 11, 96, 63, 56, 3, 84, 43, 71, 66, 92, 21, 47, 46, 18, 41, 85, 
+    84, 13, 57, 6, 8, 37, 13, 75, 80, 36, 96, 20, 77, 4, 62, 88, 83, 42, 56, 77, 27, 55, 79, 87, 99, 2, 
+    19, 8, 9, 12, 4, 60, 38, 14, 51, 26, 1, 53, 19, 42, 28, 14, 2, 57, 86, 10, 50, 63, 28, 18, 8, 3, 69, 
+    12, 11, 74, 45, 58 };
+
+//Console.WriteLine(ArrayUtils.FindMiddleValue(array));
+//Console.WriteLine(ArrayUtils.FindFirst(array, 1));
+//Console.WriteLine(ArrayUtils.FindLast(array, 1));
+//Console.WriteLine(ArrayUtils.Min(array));
+//Console.WriteLine(ArrayUtils.Max(array));
+//Console.WriteLine(ArrayUtils.Average(array));
+
+//ArrayUtils.Sort(array);
+
+//Console.WriteLine(ArrayUtils.Search(array, 9));
+
+//Console.WriteLine(ArrayUtils.Union(array, arrayGrandePraCaramba));
+//Console.WriteLine(ArrayUtils.Intersection(array, arrayGrandePraCaramba));
