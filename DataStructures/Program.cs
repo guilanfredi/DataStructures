7557dc7356a8feb6cb274258ff6e781855d770c2
Felipe Luzia <felipe.luzia@hotmail.com>
2023-02-22 20:52:57 -0300

    Aula 01 - 22/02/23

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DataStructures.Arrays;
 
+int[] arrayNull = new int[0];
 int[] array = new int[7] { 5, 3, 1, 9, 4, 8, 1 };
 int[] arrayGrandePraCaramba = new int[100] { 61, 15, 65, 82, 17, 1, 36, 34, 18, 19, 7, 50, 36, 8, 31, 5, 
     9, 57, 62, 32, 64, 92, 30, 92, 4, 9, 11, 96, 63, 56, 3, 84, 43, 71, 66, 92, 21, 47, 46, 18, 41, 85, 
@@ -7,12 +8,12 @@ int[] arrayGrandePraCaramba = new int[100] { 61, 15, 65, 82, 17, 1, 36, 34, 18, 
     19, 8, 9, 12, 4, 60, 38, 14, 51, 26, 1, 53, 19, 42, 28, 14, 2, 57, 86, 10, 50, 63, 28, 18, 8, 3, 69, 
     12, 11, 74, 45, 58 };
 
-//Console.WriteLine(ArrayUtils.FindMiddleValue(array));
+//Console.WriteLine(ArrayUtils.FindMiddleValue(arrayNull));
 //Console.WriteLine(ArrayUtils.FindFirst(array, 1));
 //Console.WriteLine(ArrayUtils.FindLast(array, 1));
 //Console.WriteLine(ArrayUtils.Min(array));
 //Console.WriteLine(ArrayUtils.Max(array));
-//Console.WriteLine(ArrayUtils.Average(array));
+Console.WriteLine(ArrayUtils.Average(array));
 
 //ArrayUtils.Sort(array);
 
